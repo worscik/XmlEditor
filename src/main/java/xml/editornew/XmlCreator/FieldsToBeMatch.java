ffbddad3abb7814b2cd092e93d7c9437b6a87c71
worscik <kaczmarskim95@gmail.com>
2022-12-07 21:15:19 +0100

    add fields to cute and match

--- src/main/java/xml/editornew/XmlCreator/FieldsToBeMatch.java
+++ src/main/java/xml/editornew/XmlCreator/FieldsToBeMatch.java
@@ -14,4 +14,17 @@ public class FieldsToBeMatch {
     public void setMatchFields(String matchFields) {
         this.matchFields = matchFields;
     }
+
+    public String fieldsToCut(String mapping){
+        switch (mapping){
+            case  "rrr/channel/item" -> {
+                return matchFields = "rrs/channel/item";
+            }
+            case "root/item" -> {
+                return matchFields = "root/item";
+            }
+        }
+        return "Cannot find fileds to match";
+    }
+
 }
